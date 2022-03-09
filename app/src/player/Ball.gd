465c1bb6b65090468b1db876e447756fffe6127a
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-03-09 17:40:18 +0200

    Update Paddle

--- app/src/player/Ball.gd
+++ app/src/player/Ball.gd
@@ -6,30 +6,12 @@ var radius: float
 var speed = 600
 
 
-#var direction = Vector3.UP
-#var position = Transform()
-#var velocity = Vector3(0,10,0)
-#
-##onready var _initial_pos = position
-
-
 func _enter_tree():
 	radius = $MeshInstance.mesh.radius
 
 
-func _ready():
-	#linear_velocity.y = speed
-	
-	#add_force(Vector3(0,10,0), Vector3(0,10,0))
-	#apply_impulse(Vector3(0,1,0), Vector3(0,10,0))
-	#print(player.get_node("Rotor/Paddle/BallPosition").transform.origin)
-	#print($MeshInstance.mesh.radius)
-	pass
-
-
-#func _physics_process(_delta):
-#	if Input.is_action_pressed("use_shot"):
-#		linear_velocity.y = speed
+#func _ready():
+#	pass
 
 
 func _input(event):
--- app/src/player/Paddle.gd
+++ app/src/player/Paddle.gd
@@ -8,22 +8,9 @@ onready var ball = player.ball
 onready var ball_position = $BallPosition
 
 
-#const MOVE_SPEED = 100
-#var _ball_dir
-#var _up
-#var _down
-#onready var _screen_size_y = get_viewport_rect().size.y
-
-
 func _ready():
 	transform.origin.y = -offset_y
 	ball_position.transform = Transform(ball_position.transform.basis, Vector3(0, offset_y + ball.radius + 0.001, 0))
-
-
-#func _process(delta):
-#	# Move up and down based on input.
-#	var input = Input.get_action_strength(_down) - Input.get_action_strength(_up)
-#	position.y = clamp(position.y + input * MOVE_SPEED * delta, 16, _screen_size_y - 16)
 
 
 #func _on_Area_area_entered(area):
