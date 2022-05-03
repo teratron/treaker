c5b2d69f039850f048efc12af6098c5a8aba3bd4
Oleg Alexandrov <teratronsirimus@gmail.com>
2022-05-03 17:06:12 +0300

    Waypoint

--- app/src/waypoint/Waypoint.gd
+++ app/src/waypoint/Waypoint.gd
@@ -16,28 +16,27 @@ func _ready():
 
 
 func _process(_delta):
+	var viewport := get_viewport()
+	
 	if !camera.current:
-		camera = get_viewport().get_camera()
+		camera = viewport.get_camera()
 	
 	var parent_translation   = parent.global_transform.origin
 	var camera_transform     = camera.global_transform
 	var camera_translation   = camera_transform.origin
 	var unprojected_position = camera.unproject_position(parent_translation)
 	var is_behind = camera_transform.basis.z.dot(parent_translation - camera_translation) > 0
-	
-	#print(camera_transform.basis.z.dot(parent_translation - camera_translation))
+	#var is_behind = camera.is_position_behind(parent_translation)
 	
 	if !sticky:
 		rect_position = unprojected_position
 		visible = !is_behind
 		return
 	
-	var distance = camera_translation.distance_to(parent_translation)
-	modulate.a = clamp(range_lerp(distance, 0, 2, 0, 1), 0, 1)
-	
-	var viewport_base_size = (
-			get_viewport().get_size_override() if get_viewport().get_size_override() > Vector2(0, 0)
-			else get_viewport().size
+	var viewport_base_size = viewport.get_size_override()
+	viewport_base_size = (
+			viewport_base_size if viewport_base_size > Vector2(0, 0)
+			else viewport.size
 		)
 	
 	if is_behind:
@@ -46,12 +45,6 @@ func _process(_delta):
 		else:
 			unprojected_position.x = MARGIN
 	
-	# For the screen's Y axis, the projected position is NOT useful to us
-	# because we don't want to indicate to the user that they need to look
-	# up or down to see something behind them. Instead, here we approximate
-	# the correct position using difference of the X axis Euler angles
-	# (up/down rotation) and the ratio of that with the camera's FOV.
-	# This will be slightly off from the theoretical "ideal" position.
 	if is_behind || unprojected_position.x < MARGIN || unprojected_position.x > viewport_base_size.x - MARGIN:
 		var look = camera_transform.looking_at(parent_translation, Vector3.UP)
 		var diff = angle_diff(look.basis.get_euler().x, camera_transform.basis.get_euler().x)
@@ -62,29 +55,44 @@ func _process(_delta):
 			clamp(unprojected_position.y, MARGIN, viewport_base_size.y - MARGIN)
 		)
 	
-	#label.visible = true
-	rect_rotation = 0
-	var overflow = 0
+	var overflow := 0.0
+	var rotation := 0.0
 	
 	if rect_position.x <= MARGIN:
 		# Left overflow.
 		overflow = -45
-		#label.visible = false
-		rect_rotation = 90
+		rotation = 90
 	elif rect_position.x >= viewport_base_size.x - MARGIN:
 		# Right overflow.
 		overflow = 45
-		#label.visible = false
-		rect_rotation = 270
+		rotation = 270
 	
 	if rect_position.y <= MARGIN:
 		# Top overflow.
-		#label.visible = false
-		rect_rotation = 180 + overflow
+		rotation = 180 + overflow
 	elif rect_position.y >= viewport_base_size.y - MARGIN:
 		# Bottom overflow.
-		#label.visible = false
-		rect_rotation = -overflow
+		rotation = -overflow
+	
+	rect_rotation = rotation
+	
+	
+#	prints(camera.is_position_behind(parent_translation))
+#	visible = not camera.is_position_behind(parent_translation)
+#	rect_position = camera.unproject_position(parent_translation)
+
+	
+	
+#	if unprojected_position > Vector2(0, 0) && unprojected_position < get_viewport().size:
+#		visible = false
+#	else:
+#		visible = true
+	
+#	if unprojected_position.x >= 0 && unprojected_position.x <= get_viewport().size.x && \
+#		unprojected_position.y >= 0 && unprojected_position.y <= get_viewport().size.y:
+#		visible = false
+#	else:
+#		visible = true
 
 
 static func angle_diff(from, to):
